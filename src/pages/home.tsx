98496d2a7186ecb4141fcfc96baf3f1787d50adf
WellingtonLFaria <wellingtonll.faria@gmail.com>
2023-12-11 12:00:01 -0300

    Adicionando links

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -9,9 +9,9 @@ export default function Home() {
             <h2 className='text-2xl md:text-3xl lg:text-4xl'>Desenvolvedor de Software</h2>
             <hr className="w-7/12 md:w-6/12 lg:w-5/12 border-t border-white mt-2" />
             <div className="w-6/12 md:w-5/12 lg:w-4/12 flex justify-between mt-2">
-                <img src={instagram} alt="Ícone instagram" className='w-10' />
-                <img src={linkedin} alt="Ícone LinkedIn" className='w-10' />
-                <img src={github} alt="Ícone GitHub" className='w-10'/>
+                <img src={instagram} alt="Ícone instagram" className='w-10 cursor-pointer' onClick={() => {window.open("https://www.instagram.com/wellingtonfarial/")}}/>
+                <img src={linkedin} alt="Ícone LinkedIn" className='w-10 cursor-pointer' onClick={() => {window.open("https://www.linkedin.com/in/wellington-luiz-de-faria-92007425b/")}}/>
+                <img src={github} alt="Ícone GitHub" className='w-10 cursor-pointer'onClick={() => {window.open("https://github.com/WellingtonLFaria")}}/>
             </div>
         </>
     );
